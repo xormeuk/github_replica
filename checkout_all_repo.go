0c07b7b9eb162737bb2f69748121875374b7c9dd
Matt Brocklehurst <m@xor.me.uk>
2024-03-23 09:08:31 +0000

    Fixed a bug where we were not checking out private repos

--- checkout_all_repo.go
+++ checkout_all_repo.go
@@ -3,71 +3,70 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 )
 
-// Repository structure to match JSON data from GitHub API
+// Repository represents the minimal data for a GitHub repository we need.
 type Repository struct {
 	Name   string `json:"name"`
 	SshURL string `json:"ssh_url"`
 }
 
-func checkoutRepositories(username, token, destinationDir string) {
-	url := fmt.Sprintf("https://api.github.com/users/%s/repos", username)
+func checkoutRepositories(githubToken, destinationDir string) {
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", "https://api.github.com/user/repos", nil)
 	if err != nil {
-		fmt.Printf("Failed to create request: %v\n", err)
-		return
+		log.Fatalf("Failed to create request: %v", err)
 	}
 
-	req.Header.Add("Authorization", "token "+token)
+	req.Header.Add("Authorization", "token "+githubToken)
 	req.Header.Add("Accept", "application/vnd.github.v3+json")
 
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("Failed to fetch repositories: %v\n", err)
-		return
+		log.Fatalf("Failed to fetch repositories: %v", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		body, _ := ioutil.ReadAll(resp.Body)
-		fmt.Printf("Failed to fetch repositories: received status code %d\n%s\n", resp.StatusCode, string(body))
-		return
+		log.Fatalf("Failed to fetch repositories: %s", resp.Status)
 	}
 
-	var repositories []Repository
-	if err := json.NewDecoder(resp.Body).Decode(&repositories); err != nil {
-		fmt.Printf("Failed to decode response: %v\n", err)
-		return
+	var repos []Repository
+	if err := json.NewDecoder(resp.Body).Decode(&repos); err != nil {
+		log.Fatalf("Failed to decode response: %v", err)
 	}
 
-	for _, repo := range repositories {
+	for _, repo := range repos {
 		fmt.Printf("Cloning repository: %s\n", repo.Name)
-		cmd := exec.Command("git", "clone", repo.SshURL, filepath.Join(destinationDir, repo.Name))
-		if err := cmd.Run(); err != nil {
-			fmt.Printf("Failed to clone repository: %v\n", err)
+		repoPath := filepath.Join(destinationDir, repo.Name)
+
+		// Clone repo into destinationDir/repo.Name
+		cmd := exec.Command("git", "clone", repo.SshURL, repoPath)
+		err := cmd.Run()
+		if err != nil {
+			log.Printf("Failed to clone repository %s: %v", repo.Name, err)
+			continue
 		}
+		fmt.Printf("Successfully cloned %s into %s\n", repo.Name, repoPath)
 	}
 }
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: go run checkout_all_repo.go GITHUB_USERNAME destination_dir")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run checkout_all_repo.go <destination_dir>")
 		os.Exit(1)
 	}
 
-	username := os.Args[1]
-	token := os.Getenv("GITHUB_TOKEN")
-	if token == "" {
-		fmt.Println("GITHUB_TOKEN environment variable is not set.")
-		os.Exit(1)
+	githubToken := os.Getenv("GITHUB_TOKEN")
+	if githubToken == "" {
+		log.Fatal("GITHUB_TOKEN environment variable is not set")
 	}
-	destinationDir := os.Args[2]
-	checkoutRepositories(username, token, destinationDir)
+
+	destinationDir := os.Args[1]
+	checkoutRepositories(githubToken, destinationDir)
 }
