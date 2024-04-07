d10faf1500efee134a20404c62b44f3c21ef4c29
Matt Brocklehurst <mpbrocklehurst@icloud.com>
2024-04-07 17:22:58 +0100

    Added update-exisiting flag to pull any changes even if the repo has been checked out

--- github_replica.go
+++ github_replica.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,6 +18,37 @@ var GitCommitHash string
 type Repository struct {
 	Name   string `json:"name"`
 	SshURL string `json:"ssh_url"`
+}
+
+var updateExisting = flag.Bool("update-existing", false, "If set, will update existing repositories by pulling or cloning as necessary.")
+
+func checkoutOrUpdateRepository(repo Repository, destinationDir string) {
+	repoPath := filepath.Join(destinationDir, repo.Name)
+	if _, err := os.Stat(repoPath); os.IsNotExist(err) {
+		fmt.Printf("Cloning repository: %s\n", repo.Name)
+		cmd := exec.Command("git", "clone", repo.SshURL, repoPath)
+		if err := cmd.Run(); err != nil {
+			log.Printf("Failed to clone repository %s: %v", repo.Name, err)
+		} else {
+			fmt.Printf("Successfully cloned %s into %s\n", repo.Name, repoPath)
+		}
+	} else {
+		if *updateExisting {
+			fmt.Printf("Updating repository: %s\n", repo.Name)
+			cmd := exec.Command("git", "-C", repoPath, "fetch", "--all")
+			cmd.Run() // Fetch changes
+			cmd = exec.Command("git", "-C", repoPath, "reset", "--hard", "origin/master")
+			cmd.Run() // Reset any local changes
+			cmd = exec.Command("git", "-C", repoPath, "pull")
+			if err := cmd.Run(); err != nil {
+				log.Printf("Failed to update repository %s: %v", repo.Name, err)
+			} else {
+				fmt.Printf("Successfully updated %s\n", repo.Name)
+			}
+		} else {
+			fmt.Printf("Warning: Repository '%s' already exists and '-update-existing' not set\n", repo.Name)
+		}
+	}
 }
 
 func checkoutRepositories(githubToken, destinationDir string) {
@@ -45,29 +77,19 @@ func checkoutRepositories(githubToken, destinationDir string) {
 	}
 
 	for _, repo := range repos {
-		fmt.Printf("Cloning repository: %s\n", repo.Name)
-		repoPath := filepath.Join(destinationDir, repo.Name)
-
-		// Clone repo into destinationDir/repo.Name
-		cmd := exec.Command("git", "clone", repo.SshURL, repoPath)
-		err := cmd.Run()
-		if err != nil {
-			log.Printf("Failed to clone repository %s: %v", repo.Name, err)
-			continue
-		}
-		fmt.Printf("Successfully cloned %s into %s\n", repo.Name, repoPath)
+		checkoutOrUpdateRepository(repo, destinationDir)
 	}
 }
 
 func main() {
+	flag.Parse()
 	fmt.Println("GitHub Replica\nMatt Brocklehurst / www.xor.me.uk")
-	// Print the GitCommitHash only if it's set
 	if GitCommitHash != "" {
 		fmt.Printf("Git Commit Hash: %s\n", GitCommitHash)
 	}
 
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run github_replica.go <destination_dir>")
+	if len(flag.Args()) != 1 {
+		fmt.Println("Usage: go run github_replica.go [-update-existing] <destination_dir>")
 		os.Exit(1)
 	}
 
@@ -76,6 +98,6 @@ func main() {
 		log.Fatal("GITHUB_TOKEN environment variable is not set")
 	}
 
-	destinationDir := os.Args[1]
+	destinationDir := flag.Arg(0) // Assumes the non-flag argument is the destination directory
 	checkoutRepositories(githubToken, destinationDir)
 }
